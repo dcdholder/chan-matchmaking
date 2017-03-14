5b78fe00be8d310be34c0ccfb1ded22563443b2a
dcdholder <dcdholder@gmail.com>
2017-03-14 17:19:44 -0400

    Rewriting the chart image and chart data classes.

--- Cell.py
+++ Cell.py
@@ -1,19 +1,96 @@
 #it looks like the coordinate is simply a tuple
 #a cell may have the color 'unselected', 'invalid', or one of six others
 
+class ColorDataField:
+	__unselectedColor   = 0xFFFFFF
+	__singleSelectIndex = 2
+	__neutralIndex      = 3
+	__colorNames = ['pink', 'blue', 'green', 'yellow', 'orange', 'red']
+	__colorCodes = [0xFF00FF, 0x0000FF, 0x00FF00, 0xFFFF00, 0xFF7200, 0xFF0000]
+	__importanceScoreMapping = [-1.0, -0.5, 0.0, 0.33, 0.67, 1.0]
+	__traitScoreMapping      = [-1.0, -0.5, 0.0, 0.5, 1.0]
+	
+	def __init__(self, colorCode, isYou, isMulticolor)
+		self.isYou        = isYou #tells us whether this is a trait or importance score
+		self.isMulticolor = isMulticolor
+		self.isSelected   = False
+		self.colorScore   = colorScoreFromCode(colorCode) #TODO: would be nice to have an alternate constructor for colorIndex rather than colorCode...
+		
+		if !isYou and !isMulticolor
+			raise ValueError('Them cells are always multicolor.')
+		
+	def colorScoreFromCode(colorCode)
+		for i in range(0,len(colorValues))
+			if __colorCodes[i]==colorCode
+				self.isSelected = True
+				return i
+		
+		if colorCode==__unselectedColor
+			if isMulticolor
+				self.isSelected = True
+				return __neutralIndex #for multicolor cells, an unfilled cell can be assumed yellow
+			else
+				self.isSelected = False
+				return 0
+		
+		raise ValueError('Could not map color code ' + colorCode + 'to a valid color score.')
+		
+	def singleColorTraitSelected()
+		return __colorNames[self.colorScore]=='green'
+		
+	def yourTraitVsTheirImportance(theirImportance)
+		if !isYou
+			raise ValueError('Must be executed on a You color data field.')
+		
+		if isMulticolor
+			return multiColorYouScoring(theirImportance.colorScore)
+		else
+			return singleColorYouScoring(singleColorTraitSelected(),theirImportance.colorScore)
+		
+	def multiColorYouScoring(importanceScoreIndex) #min possible score is 0, max possible score is 1.0	
+		importanceScore = __importanceScoreMapping[importanceScoreIndex]
+		traitScore      = __traitScoreMapping[self.colorScore]
+
+		scoreDelta = importanceScore - traitScore
+		if importanceScore*traitScore > 0: #same sign
+			scoreDelta=abs(scoreDelta)
+		else:
+			scoreDelta=-abs(scoreDelta)
+
+		finalScore = scoreDelta * importanceScore #increases importance of delta for extreme scores, makes score 0 if importance is neutral
+		finalScore = (finalScore+1.0) / 2.0       #normalize final answer to a range between 0 and 1.0
+	
+		return finalScore
+	
+	def singleColorYouScoring(traitSelected,importanceScoreIndex) #trait score is either 1.0 or 0.0, no in-betweens
+		if traitSelected:
+			traitScoreIndex = len(__traitScoreMapping)-1 #just set to either extreme trait score
+		else:
+			traitScoreIndex = 0
+		
+		return multiColorYouScoring(traitScoreIndex,importanceScoreIndex)
+
+class Cell: #an indivisble component of an image element
+	def __init__(self,pixelMap,coordinates)
+		self.pixelMap       = pixelMap
+		self.coordinates    = coordinates
+		self.colorDataField = self.__setColor()
+	
+	def __setColor() #just select color from the given coordinates -- is different for square cell
+		#TODO: figure out how to handle invalid cell colors
+		#TODO: write ImageMagick code to get color from pixel
+		return ColorDataField(colorCode)
+			
+
 class PictographicCell(Cell):
 	
 
-class Cell
-	__unselectedColor = 0xFFFFFF
-	__colorValues = {'pink': 0xFFFFFF, 'blue': 0xFFFFFF, 'green': 0xFFFFFF, 'yellow': 0xFFFFFF, 'orange': 0xFFFFFF, 'red': 0xFFFFFF}
-	__colorScores = {'pink': 5, 'blue': 4, 'green': 3, 'yellow': 2, 'orange': 1, 'red': 0}
-
+class SquareCell(Cell):
 	def __init__(self, pixelMap, coordinates, size)
-		self.pixelMap    = pixelMap
-		self.coordinates = coordinates
-		self.size        = size
-		self.color       = self.__setColor()
+		self.pixelMap       = pixelMap
+		self.coordinates    = coordinates
+		self.size           = size
+		self.colorDataField = self.__setColor()
 	
 	def __setColor() #this guy samples the color and assigns it the correct color name string
 		centerPixelCoordinates = self.getCenterPixel()
@@ -57,19 +134,16 @@ class Cell
 		middleY = self.coordinates['y'] + self.size['y'] / 2
 
 		return {'x': middleX, 'y': middleY}
+
+class Element #a chart element is a collection of individual cells
 		
-class Checkbox
-	def __init__(self, pixelMap, coordinates, size, label)
-		self.cell  = Cell(pixelMap, coordinates, size)
-		self.label = label
-		
-class CheckboxSet
+class CheckboxSet(Element):
 	def __init__(self, checkboxes, category, label)
 		self.checkboxes = checkboxes
 		self.category   = category
 		self.label      = label
 
-class BooleanBar
+class BooleanBar(Element):
 	def __init__(self, pixelMap, coordinates, size, yesPosition, category, label)
 		self.pixelMap    = pixelMap
 		self.coordinates = coordinates
@@ -90,7 +164,7 @@ class BooleanBar
 			self.yesCell = Cell(pixelMap, rightCoordinates, cellSize)
 			self.noCell  = Cell(pixelMap, coordinates, cellSize)
 
-class NumericalRangeBar
+class NumericalRangeBar(Element):
 	#generate the cells
 	def __init__(self, pixelMap, coordinates, cellSize, numCells, minValue, maxValue, category, label)
 		self.pixelMap    = pixelMap
@@ -107,7 +181,7 @@ class NumericalRangeBar
 		#check if a single cell is selected, two adjacent cells only, or all cells up to a final cell (and no other cells beyond that)
 		#throw an exception if any weirdness is encountered
 
-class FuzzyRangeBar
+class FuzzyRangeBar(Element):
 	def __init__(self, pixelMap, coordinates, cellSize, numCells, leftQuality, rightQuality, category, label)
 		self.pixelMap     = pixelMap
 		self.coordinates  = coordinates
@@ -122,34 +196,13 @@ class FuzzyRangeBar
 	
 	def getPercentScoreRight()
 		#TODO: generate a key-value pair corresponding to the "matching percentage" of the right attribute
+
+class Category:
+	def __init__(self, name, elements)
+		self.name     = name
+		self.elements = elements
 		
 class Chart
-	def __init__(self, filename, categoryWeightings)
-		#pixelMap = #TODO: generate the pixel map
-		self.you  = Target(pixelMap,youCheckboxSets,youNumericalRangeBars,youFuzzyRangeBars)
-		self.them = Target(pixelMap,themCheckboxSets,themNumericalRangeBars,themFuzzyRangeBars)
-		self.categoryWeightings = categoryWeightings
-		
-	def scoreAllCategories(otherChart)
-		totalScore = 0.0
-		for category, weighting in self.categoryWeightings
-			totalScore += scoreCategory(category,otherChart)*weighting
-			
-		return totalScore	
-
-	#TODO: very ugly, fix the checkbox and bar classes
-	def scoreCategory(category, otherChart)
-		#whatever.compare should do the weighting
-		for checkboxSet in you.checkboxSetsFromCategory(category)
-			checkboxSetScore = checkboxSet.compare(otherChart.them.checkboxSetFromLabel(checkboxSet.label))
-		
-		for numericalRangeBar in you.numericalRangeBarsFromCategory(category)
-			numericalRangeBarScore = numericalRangeBar.compare(otherChart.them.numericalRangeBarFromLabel(numericalRangeBar.label))		
-			
-		for fuzzyRangeBar in you.fuzzyRangeBarsFromCategory(category)
-			fuzzyRangeBar.compare(otherChart.them.fuzzyRangeBarFromLabel(fuzzyRangeBar.label))
-
-		return checkboxSetScore + numericalRangeBarScore + fuzzyRangeBar
-
-class Target
-	
+	def __init__(self, name, categories)
+		self.name     = name
+		self.elements = categories
