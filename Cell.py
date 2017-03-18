df0796a3ba45c0d6534fcc632d4e85d7536ca0ee
dcdholder <dcdholder@gmail.com>
2017-03-18 14:14:44 -0400

    Filled out and added some Element classes.

--- Cell.py
+++ Cell.py
@@ -1,6 +1,8 @@
 #it looks like the coordinate is simply a tuple
 #a cell may have the color 'unselected', 'invalid', or one of six others
 
+#TODO: determine whether color testing needs some "fuzziness" by testing with multiple jpeg qualities
+
 import yaml
 
 class ColorDataField:
@@ -54,7 +56,7 @@ class ColorDataField:
 		traitScore      = __traitScoreMapping[self.colorScore]
 
 		scoreDelta = importanceScore - traitScore
-		if importanceScore*traitScore > 0: #same sign
+		if importanceScore*traitScore > 0: #true if they're the same sign
 			scoreDelta=abs(scoreDelta)
 		else:
 			scoreDelta=-abs(scoreDelta)
@@ -72,120 +74,217 @@ class ColorDataField:
 		
 		return multiColorYouScoring(traitScoreIndex,importanceScoreIndex)
 
+#Cells
 class Cell: #an indivisble component of an image element
-	def __init__(self,pixelMap,coordinates)
+	def __init__(self,label,pixelMap,coordinates)
 		self.pixelMap       = pixelMap
 		self.coordinates    = coordinates
 		self.colorDataField = self.__setColor()
 	
 	def __setColor() #just select color from the given coordinates -- is different for square cell
 		#TODO: figure out how to handle invalid cell colors
-		centerPixelCoordinates = self.getCenterPixel()
+		centerPixelCoordinates = self.__getCenterPixel()
 		
 		rgb          = self.pixelMap.getPixel(centerPixelCoordinates)
 		hexColorCode = '#{:02x}{:02x}{:02x}'.format(rgb[0],rgb[1],rgb[2])
 		
 		return ColorDataField(hexColorCode)
 	
-	def getCenterPixel(): #the default is just to return the 'coordinates' -- overriden in SquareCell, but not PictographicCell
+	def __getCenterPixel(): #the default is just to return the 'coordinates' -- overriden in SquareCell, but not PictographicCell
 		return coordinates
 	
-	def fillCell(coordinates, colorCode)		
+	def fillCell(coordinates, colorCode):
+		pass	
 
-class PictographicCell(Cell):
-	
+#TODO: allow for multiple color sampling points, since users don't fill in pictographic cells all the same way
+class PictographicCell(Cell): #subclass is really just for readability right now
+	def __init__(self,label,pixelMap,coordinates)
+		super().__init__(self,label,pixelMap,coordinates)
 
 class SquareCell(Cell):
-	def __init__(self, pixelMap, coordinates, size)
-		self.pixelMap       = pixelMap
-		self.coordinates    = coordinates
-		self.size           = size
-		self.colorDataField = self.__setColor()
+	def __init__(self,label,pixelMap,coordinates,size)
+		self.size = size
+		super().__init__(self,label,pixelMap,coordinates)
 	
 	def genRow(pixelMap,baseCoordinates,cellSize,numCells) #create a horizontal row of cells
 		cells = []
-		for i in range(numCells)
+		for i in range(0,numCells)
 			cellCoordinates[0] = baseCoordinates[0] + cellSize[0] * i
 			cellCoordinates[1] = baseCoordinates[1]
-			cells.append(Cell(pixelMap,cellCoordinates,cellSize))
+			cells[i] = SquareCell(pixelMap,cellCoordinates,cellSize)
 			
 		return cells
 	
-	def getCenterPixel()
+	def genSquare(pixelMap,coordinates,cellSize,cellDimensions)
+		for j in range(0,cellDimensions) #assume all 2D cell arrays are square
+			for i in range(0,cellDimensions)
+				cells[j][i] = SquareCells(pixelMap,cellCoordinates,cellSize)
+	
+	def __getCenterPixel()
 		middleX = self.coordinates[0] + self.size[0] / 2
 		middleY = self.coordinates[1] + self.size[1] / 2
 
 		return (middleX, middleY)
-	
-class CheckboxSet(Element):
-	def __init__(self, checkboxes, category, label)
-		self.checkboxes = checkboxes
-		self.category   = category
-		self.label      = label
 
-class BooleanBar(Element):
-	def __init__(self, pixelMap, coordinates, size, yesPosition, category, label)
+#Elements
+class Element: #a chart element is a collection of individual cells, has a non-configurable weighting within a category
+	def __init__(self, elementYaml, pixelMap, isYou)
+		self.elementYaml = elementYaml
+		self.name        = elementYaml['name']
+		self.weighting   = elementYaml['weighting']
 		self.pixelMap    = pixelMap
-		self.coordinates = coordinates
-		self.size        = size
-		self.category    = category
-		self.label       = label
+		self.isYou       = isYou #specify whether to get 'You' or 'Them'
+		self.cells       = self.__getCells()
+
+	def __youOrThemString():
+		if self.isYou:
+			return 'you'
+		else:
+			return 'them'
+
+	def __coordinatesFromString(coordinates):
+		coordsAsStringArray = coordinates.split("x")
+		coordsAsIntTuple    = tuple(list(map(int,coordsAsStringArray)))
+
+	def getYouAndThemElementsFromYaml(elementYaml)
+		pass
+
+	def __getCells():
+		pass
+
+#TODO: figure out how abstract classes work in Python
+class CheckboxSet(Element):
+	def __init__(self, elementYaml, pixelMap, isYou)
+		super().__init__(self,elementYaml,pixelMap,isYou)
+
+class PictographicCheckboxSet(CheckboxSet):
+	def __init__(self, elementYaml, pixelMap, isYou)
+		super().__init__(self,elementYaml,pixelMap,isYou)
+	
+	def __getCells():
+		cells = {}
+		for cellYaml in elementYaml['checkboxes']
+			label           = cellYaml['label']
+			cellCoordinates = cellYaml['coordinates'][__youOrThemString()]
+			cells[cellYaml['label']] = PictographicCell(self,label,pixelMap,cellCoordinates)
+
+class SquareCheckboxSet(CheckboxSet):
+	def __init__(self, elementYaml, pixelMap, isYou)
+		super().__init__(self,elementYaml,pixelMap,isYou)
+		self.isMulticolor = self.elementYaml['isMulticolor']
+		self.checkboxSize = self.elementYaml['size']
+	
+	#TODO: DRY
+	def __getCells():
+		cells = {}
+		for cellYaml in elementYaml['checkboxes']
+			label           = cellYaml['label']
+			cellCoordinates = cellYaml['coordinates'][__youOrThemString()]
+			cells[cellYaml['label']] = SquareCell(self,label,pixelMap,cellCoordinates,checkboxSize)
+			
+		return cells
+
+#TODO: figure out how abstract classes work in Python
+class Bar(Element):
+	def __init__(self, elementYaml, pixelMap, isYou)
+		super().__init__(self,elementYaml,pixelMap,isYou)
+		self.coordinates = elementYaml['coordinates'][__youOrThemString()]
+		self.size        = elementYaml['size']
+
+class BooleanBar(Bar):
+	def __init__(self, elementYaml, pixelMap, isYou)
+		super().__init__(self,elementYaml,pixelMap,isYou)
+		self.yesPosition = elementYaml['yesPosition']
+			
+	def __getCells():
+		cells = {}
+
+		cellSize = ()
+		cellSize[0] = self.size[0] / 2
+		cellSize[1] = self.size[1]
 		
-		cellSize[0] = size[0] / 2
-		cellSize[1] = size[1]
-		
+		rightCoordinates = ()
 		rightCoordinates[0] = coordinates[0] / cellSize[0]
 		rightCoordinates[1] = coordinates[1]
 		
 		if yesPosition=='left'
-			self.yesCell = Cell(pixelMap, coordinates, cellSize)
-			self.noCell  = Cell(pixelMap, rightCoordinates, cellSize)
+			yesCell = SquareCell(pixelMap, coordinates, cellSize)
+			noCell  = SquareCell(pixelMap, rightCoordinates, cellSize)
 		else
-			self.yesCell = Cell(pixelMap, rightCoordinates, cellSize)
-			self.noCell  = Cell(pixelMap, coordinates, cellSize)
+			yesCell = SquareCell(pixelMap, rightCoordinates, cellSize)
+			noCell  = SquareCell(pixelMap, coordinates, cellSize)
+		
+		cells['yes'] = yesCell
+		cells['no']  = noCell
+		
+		return cells
 
-class NumericalRangeBar(Element):
-	#generate the cells
-	def __init__(self, pixelMap, coordinates, cellSize, numCells, minValue, maxValue, category, label)
-		self.pixelMap    = pixelMap
-		self.coordinates = coordinates
-		self.minValue    = minValue
-		self.maxValue    = maxValue
-		self.cells       = Cell.genRow(pixelMap,coordinates,cellSize,numCells)
-		self.category    = category
-		self.label       = label
+#TODO: DRY -- numerical, binary, fuzzy might be better suited as subclasses of a 'bar' class
+class NumericalRangeBar(Bar):
+	def __init__(self, elementYaml, pixelMap, isYou)
+		super().__init__(self,elementYaml,pixelMap,isYou)
+		self.numCells     = elementYaml['numCells']
+		self.leftQuality  = elementYaml['min']
+		self.rightQuality = elementYaml['max']
+	
+	#TODO: DRY
+	def __getCells():
+		cells     = {}
+		cellArray = SquareCell.genRow(pixelMap,coordinates,cellSize,numCells)
+		
+		for i in range(0,len(cellArray))
+			cells[i] = cellArray[i] #TODO: maybe there's a better way to do this than with a for loop
 			
+		return cells
+	
 	def getNumericalValue()
 		#TODO:
 		#go through cells from left to right, find the max which has been selected
 		#check if a single cell is selected, two adjacent cells only, or all cells up to a final cell (and no other cells beyond that)
 		#throw an exception if any weirdness is encountered
 
-class FuzzyRangeBar(Element):
-	def __init__(self, pixelMap, coordinates, cellSize, numCells, leftQuality, rightQuality, category, label)
-		self.pixelMap     = pixelMap
-		self.coordinates  = coordinates
-		self.leftQuality  = leftQuality
-		self.rightQuality = rightQuality
-		self.cells        = Cell.genRow(pixelMap,coordinates,cellSize,numCells)
-		self.category     = category
-		self.label        = label
+class FuzzyRangeBar(Bar):
+	def __init__(self, elementYaml, pixelMap, isYou)
+		super().__init__(self,elementYaml,pixelMap,isYou)
+		self.numCells     = elementYaml['numCells']
+		self.leftQuality  = elementYaml['left']
+		self.rightQuality = elementYaml['right']
+	
+	def __getCells():
+		cells     = {}
+		cellArray = SquareCell.genRow(pixelMap,coordinates,cellSize,numCells)
 		
+		for i in range(0,len(cellArray))
+			cells[str(i)] = cellArray[i] #TODO: maybe there's a better way to do this than with a for loop
+	
+		return cells
+	
 	def getPercentScoreLeft()
+		pass
 		#TODO: generate a key-value pair corresponding to the "matching percentage" of the left attribute
 	
 	def getPercentScoreRight()
+		pass
 		#TODO: generate a key-value pair corresponding to the "matching percentage" of the right attribute
 
-class Element: #a chart element is a collection of individual cells, has a non-configurable weighting within a category
-	def __init__(self, elementYaml, weighting, pixelMap)
-		self.elementYaml = elementYaml
-		self.name        = elementYaml['name']
-		self.weighting   = weighting
-		self.pixelMap    = pixelMap
-		self.cells       = self.__getCells()
-
+class TwoDFuzzyRangeBar(Bar):
+	def __init__(self, elementYaml, pixelMap, isYou)
+		super().__init__(self,elementYaml,pixelMap,isYou)
+		self.numCells      = elementYaml['cellDimensions']
+		self.leftQuality   = elementYaml['free']
+		self.rightQuality  = elementYaml['regulated']
+		self.topQuality    = elementYaml['capitalist']
+		self.bottomQuality = elementYaml['socialist']
+		
 	def __getCells():
+		cells       = {}
+		cell2DArray = SquareCell.genSquare(pixelMap,coordinates,cellSize,cellDimensions)
+		
+		for j in range(0,len(cell2DArray)):
+			for i in range(0,len(cell2DArray[0])):
+				cells[str(i) + "," + str(j)] = cell2DArray[j][i]
+		
+		return cells
 
 class Category:
 	def __init__(self, categoryYaml, weighting, pixelMap) #category weighting must be passed in by chart, not set in stone
